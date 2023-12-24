0ea990ca6c837d4d19e1694c172eecdd7a7a727d
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2023-12-24 09:34:09 -0600

    initial commits for days 20-25

---
+++ aoc2023/src/day25/day25.test.js
@@ -0,0 +1,31 @@
+import fs from "fs"
+import {part1, part2, exampleInput} from './index.ts'
+
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
+
+describe('Day25Test tests', () => {
+  describe('part1', () => {
+    describe('example input', () => {
+      it('part1 should be...', () => {
+        expect(part1(exampleInput)).toBe()
+      })
+    })
+    describe('real input', () => {
+      it.skip('part1 should be...', () => {
+        expect(part1(input)).toBe()
+      })
+    })
+  })
+  describe('part2', () => {
+    describe('example input', () => {
+      it.skip('part2 should be...', () => {
+        expect(part2(exampleInput)).toBe()
+      })
+    })
+    describe('real input', () => {
+      it.skip('part2 should be...', () => {
+        expect(part2(input)).toBe()
+      })
+    })
+  })
+})
