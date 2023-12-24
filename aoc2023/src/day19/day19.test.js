8bc95428261f753e88a68cbe05e6ffb034eaf9f5
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2023-12-24 09:28:56 -0600

    day19 part 1

---
+++ aoc2023/src/day19/day19.test.js
@@ -0,0 +1,85 @@
+import fs from "fs"
+import {part1, part2, exampleInput, parseInput, sumUpAcceptedPartRatings, isAcceptedPart} from './index.ts'
+
+const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
+
+describe('Day19Test tests', () => {
+  describe('part1', () => {
+    describe('example input', () => {
+      it('should find 11 workflows', () => {
+        expect(parseInput(exampleInput).workflows).toHaveLength(11)
+      })
+      describe('first workflow', () => {
+        let firstWorkflow
+        beforeEach(() => {
+          firstWorkflow = parseInput(exampleInput).workflows[0]
+        })
+        it('should find the first workflow to have the name px', () => {
+          expect(firstWorkflow.name).toBe('px')
+        })
+        it('should find the first workflow to have three rules', () => {
+          expect(firstWorkflow.rules).toHaveLength(3)
+        })
+        it('should find the first rule of the first workflow to have nextWorkflow of qkq', () => {
+          expect(firstWorkflow.rules[0].nextWorkflow).toBe('qkq')
+        })
+        it('should find the last rule of the first workflow to have nextWorkflow of rfg', () => {
+          expect(firstWorkflow.rules[firstWorkflow.rules.length - 1].nextWorkflow).toBe('rfg')
+        })
+      })
+      describe('first partRating', () => {
+        let firstPartRating
+        beforeEach(() => {
+          firstPartRating = parseInput(exampleInput).partRatings[0]
+        })
+        it('should have correct xmas values', () => {
+          expect(firstPartRating).toEqual({x: 787, m: 2655, a: 1222, s: 2876})
+        })
+      })
+      it('should find 5 partRatings', () => {
+        expect(parseInput(exampleInput).partRatings).toHaveLength(5)
+      })
+      describe('when summing up accepted part ratings', () => {
+        it('should return 19114', () => {
+          expect(sumUpAcceptedPartRatings([
+            {x: 787, m: 2655, a: 1222, s: 2876},
+            {x: 2036, m: 264, a: 79, s: 2244},
+            {x: 2127, m: 1623, a: 2188, s: 1013},
+          ])).toBe(19114)
+        })
+      })
+      describe('when checking if isAcceptedPart', () => {
+
+        it('should accept the correct parts', () => {
+          expect(isAcceptedPart({x: 787, m: 2655, a: 1222, s: 2876}, parseInput(exampleInput).workflows)).toBe(true)
+          expect(isAcceptedPart({x: 2036, m: 264, a: 79, s: 2244}, parseInput(exampleInput).workflows)).toBe(true)
+          expect(isAcceptedPart({x: 2127, m: 1623, a: 2188, s: 1013}, parseInput(exampleInput).workflows)).toBe(true)
+        })
+        it('should not accept the rejected parts', () => {
+          expect(isAcceptedPart({x: 1679, m: 44, a: 2067, s: 496}, parseInput(exampleInput).workflows)).toBe(false)
+          // expect(isAcceptedPart({x: 2461, m: 1339, a: 446, s: 291}, parseInput(exampleInput).workflows)).toBe(false)
+        })
+      })
+      it('part1 should be...', () => {
+        expect(part1(exampleInput)).toBe(19114)
+      })
+    })
+    describe('real input', () => {
+      it('part1 should be...', () => {
+        expect(part1(input)).toBe(353553)
+      })
+    })
+  })
+  describe('part2', () => {
+    describe('example input', () => {
+      it.skip('part2 should be...', () => {
+        expect(part2(exampleInput)).toBe(167409079868000)
+      })
+    })
+    describe('real input', () => {
+      it.skip('part2 should be...', () => {
+        expect(part2(input)).toBe()
+      })
+    })
+  })
+})
--- aoc2023/src/day19/index.ts
+++ aoc2023/src/day19/index.ts
@@ -1,11 +1,128 @@
 import run from "aocrunner"
 
-export const parseInput = (rawInput: string) => rawInput.split('\n')
+export type PartRating = {
+  x: number
+  m: number
+  a: number
+  s: number
+}
+
+export type Condition = {
+  partRatingCategory: string
+  operator: string
+  rating: number
+}
+
+export type Rule = {
+  condition?: Condition,
+  nextWorkflow: string
+}
+export type Workflow = {
+  name: string
+  rules: Rule[]
+}
+
+enum Status {
+  ACCEPTED = 'A',
+  REJECTED = 'R',
+}
+
+const buildWorkflowRules = (rulesStringArray: string[]) => {
+  return rulesStringArray.map(rulesString => {
+    const ruleParts = rulesString.split(':')
+    if (ruleParts.length === 1) {
+      return {
+        nextWorkflow: ruleParts[0],
+      }
+    }
+    const nextWorkflow = ruleParts[1]
+    const conditionString = ruleParts[0]
+    const conditionMatcher = conditionString.match(/(?<partRatingCategory>[xmas])(?<operator>[<>=])(?<rating>\d+)/)
+
+    let condition: Condition | undefined
+    if (conditionMatcher) {
+      condition = {
+        partRatingCategory: conditionMatcher.groups.partRatingCategory,
+        operator: conditionMatcher.groups.operator,
+        rating: Number.parseInt(conditionMatcher.groups.rating)
+      }
+    }
+    return {
+      nextWorkflow,
+      condition,
+    }
+  })
+}
+
+const buildWorkflows = (workflowPart: string) => {
+  return workflowPart.split('\n').map(workflowString => {
+    const workflowMatch = workflowString.match(/^(?<name>.*)\{(?<rules>.*)\}$/u)
+    const rulesStringArray = workflowMatch.groups.rules.split(',')
+    const rules = buildWorkflowRules(rulesStringArray)
+    return {name: workflowMatch.groups.name, rules}
+  })
+}
+
+const buildPartRatings = (partRatingsPart: string) => {
+  return partRatingsPart.split('\n').map(partRatingString => {
+    const partRatingMatch = partRatingString.match(/^\{x=(?<xValue>\d+),m=(?<mValue>\d+),a=(?<aValue>\d+),s=(?<sValue>\d+)}$/)
+    return {
+      x: Number.parseInt(partRatingMatch.groups.xValue),
+      m: Number.parseInt(partRatingMatch.groups.mValue),
+      a: Number.parseInt(partRatingMatch.groups.aValue),
+      s: Number.parseInt(partRatingMatch.groups.sValue),
+    }
+  })
+}
+
+export const parseInput = (rawInput: string) : {workflows: Workflow[], partRatings: PartRating[]} => {
+  const inputParts= rawInput.split('\n\n')
+  const workflows = buildWorkflows(inputParts[0])
+  const partRatings = buildPartRatings(inputParts[1])
+
+  return {workflows, partRatings}
+}
+
+export const sumUpAcceptedPartRatings = (partRatings: PartRating[]) => {
+  return partRatings.reduce((sum, partRating) => {
+    sum += partRating.x + partRating.m + partRating.a + partRating.s
+    return sum
+  }, 0)
+}
+
+export const isAcceptedPart = (partRating: PartRating, workflows: Workflow[]) => {
+  // start at 'in'
+  const start = workflows.find(workflow => workflow.name === 'in')
+  let result = ''
+  let currentWorkflow = start
+  while (result !== Status.ACCEPTED && result !== Status.REJECTED) {
+    for (let i = 0; i < currentWorkflow.rules.length; i++) {
+      if (i === currentWorkflow.rules.length - 1) {
+        result = currentWorkflow.rules[i].nextWorkflow
+        currentWorkflow = workflows.find(workflow => workflow.name === result)
+        break
+      } else {
+        const currentRule = currentWorkflow.rules[i]
+        const categoryValue: number = partRating[currentRule.condition.partRatingCategory]
+        const conditionRating = currentRule.condition.rating
+        if ((currentRule.condition.operator === '<' && categoryValue < conditionRating) || (currentRule.condition.operator === '>' && categoryValue > conditionRating)) {
+          result = currentRule.nextWorkflow
+          currentWorkflow = workflows.find(workflow => workflow.name === result)
+          break
+        }
+      }
+    }
+  }
+  return result === Status.ACCEPTED
+}
 
 export const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-
-  return
+  // for each part, start at workflow "in"
+  const acceptedParts = input.partRatings.filter(partRating => {
+    return isAcceptedPart(partRating, input.workflows)
+  })
+  return sumUpAcceptedPartRatings(acceptedParts)
 }
 
 export const part2 = (rawInput: string) => {
@@ -14,14 +131,30 @@ export const part2 = (rawInput: string) => {
   return
 }
 
-export const exampleInput = ``
+export const exampleInput = `px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}`
 
 run({
   part1: {
     tests: [
        {
          input: exampleInput,
-         expected: "",
+         expected: 19114,
        },
     ],
     solution: part1,
@@ -30,7 +163,7 @@ run({
     tests: [
       {
         input: exampleInput,
-        expected: "",
+        expected: 167409079868000,
       },
     ],
     solution: part2,
