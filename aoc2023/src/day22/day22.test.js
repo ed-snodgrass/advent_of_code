e78a70299a12bfa872b59e4bc331361377482c23
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2023-12-27 19:39:13 -0600

    2023 day22 part 1

--- aoc2023/src/day22/day22.test.js
+++ aoc2023/src/day22/day22.test.js
@@ -1,18 +1,114 @@
 import fs from "fs"
-import {part1, part2, exampleInput} from './index.ts'
+import {
+  part1,
+  part2,
+  exampleInput,
+  parseInput,
+  letEmFall,
+  findSupportingBricks,
+  identifyBricksToDisintegrate
+} from './index.ts'
 
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
 describe('Day22Test tests', () => {
   describe('part1', () => {
     describe('example input', () => {
-      it('part1 should be...', () => {
-        expect(part1(exampleInput)).toBe()
+      describe('identifyBricksToDisintegrate', () => {
+        let brickSupportsMap, parsedInput
+        beforeEach(() => {
+          parsedInput = parseInput(exampleInput)
+          // const bricks = parsedInput.bricks
+          // currentBricks = parsedInput.bricks.filter(brick => brick.top === 2)
+          brickSupportsMap = letEmFall(parsedInput.bricks, parsedInput.maxZ)
+
+        })
+        it('should find 5 bricks to disintegrate', () => {
+          expect(identifyBricksToDisintegrate(brickSupportsMap, parsedInput.bricks)).toBe(5)
+        })
+      })
+      describe('findSupportingBricks', () => {
+        let parsedInput, currentBricks, bricks
+        beforeEach(() => {
+          parsedInput = parseInput(exampleInput)
+          bricks = parsedInput.bricks
+          currentBricks = parsedInput.bricks.filter(brick => brick.top === 2)
+        })
+        it('should find Brick A for Brick B', () => {
+          expect(findSupportingBricks(bricks, bricks[1])).toEqual([
+            expect.objectContaining({firstEnd: {x: 1, y: 0, z: 1}, secondEnd: {x: 1, y: 2, z: 1} })
+          ])
+        })
+        it('should find Brick A for Brick C', () => {
+          expect(findSupportingBricks(bricks, bricks[2])).toEqual([
+            expect.objectContaining({ firstEnd: {x: 1, y: 0, z: 1}, secondEnd: {x: 1, y: 2, z: 1} })
+          ])
+        })
+      })
+      describe('letEmFall', () => {
+        it('should find 6 bricks', () => {
+          let parsedInput = parseInput(exampleInput)
+          const brickMap = letEmFall(parsedInput.bricks, parsedInput.maxZ)
+          expect(brickMap.size).toBe(6)
+        })
+      })
+      describe('parseInput', () => {
+        let parsedInput, bricks
+        beforeEach(() => {
+
+          parsedInput = parseInput(exampleInput)
+          bricks = parsedInput.bricks
+        })
+        it('should find 7 bricks', () => {
+          expect(bricks).toHaveLength(7)
+        })
+        it('should have brick 0 of length 2', () => {
+          expect(bricks[0].brickLength).toBe(3)
+        })
+        it('should have direction y ', () => {
+          expect(bricks[0].direction).toBe('y')
+        })
+        it('should have brick 1 of length 2', () => {
+          expect(bricks[1].brickLength).toBe(3)
+        })
+        it('should have direction y ', () => {
+          expect(bricks[1].direction).toBe('x')
+        })
+        it('should have brick 6 of length 1', () => {
+          expect(bricks[6].brickLength).toBe(2)
+        })
+        it('should have direction y ', () => {
+          expect(bricks[6].direction).toBe('z')
+        })
+        it('should find maxX of 2', () => {
+          expect(parsedInput.maxX).toBe(2)
+        })
+        it('should find maxY of 2', () => {
+          expect(parsedInput.maxY).toBe(2)
+        })
+        it('should find maxZ of 9', () => {
+          expect(parsedInput.maxZ).toBe(9)
+        })
+      })
+      it('part1 should find 5 bricks to disintegrate', () => {
+        expect(part1(exampleInput)).toBe(5)
+      })
+      it('part1 should find 3 bricks to disintegrate with modified example', () => {
+        expect(part1(`0,0,1~0,0,2
+1,0,1~2,0,1
+1,0,2~1,0,2
+0,0,3~1,0,3`)).toBe(3)
       })
     })
     describe('real input', () => {
-      it.skip('part1 should be...', () => {
-        expect(part1(input)).toBe()
+      it('should sort by z', () => {
+        const bricks = parseInput(input).bricks
+        // console.log(bricks);
+        expect(bricks[0].top).toBe(1)
+      })
+      it('part1 should be 421', () => {
+
+        expect(part1(input)).toBe(421)
       })
     })
   })
--- aoc2023/src/day22/index.ts
+++ aoc2023/src/day22/index.ts
@@ -1,11 +1,185 @@
 import run from "aocrunner"
 
-export const parseInput = (rawInput: string) => rawInput.split('\n')
+enum Direction {
+  x = 'x',
+  y = 'y',
+  z = 'z'
+}
+
+export type CubePosition = {
+  x: number
+  y: number
+  z: number
+}
+
+export type Brick = {
+  top: number,
+  bottom: number,
+  direction: Direction,
+  firstEnd: CubePosition,
+  secondEnd: CubePosition,
+  brickLength: number,
+  supportingBricks: Brick[]
+}
+
+type BrickSupports = {
+  brick: Brick
+  supportedBricks: Brick[]
+}
+
+export const parseInput = (rawInput: string) => {
+  let maxX = 0
+  let maxY = 0
+  let maxZ = 0
+  let bricks = []
+
+  rawInput.split('\n').forEach(line => {
+    const parts = line.split('~').map(part => part.split(',').map(char => Number.parseInt(char)))
+
+    const firstX = parts[0][0]
+    const firstY = parts[0][1]
+    const firstZ = parts[0][2]
+    const secondX = parts[1][0]
+    const secondY = parts[1][1]
+    const secondZ = parts[1][2]
+
+    const firstEnd =  {x: firstX, y: firstY, z: firstZ}
+    const secondEnd =  {x: secondX, y: secondY, z: secondZ}
+    maxX = Math.max(maxX, firstX, secondX)
+    maxY = Math.max(maxY, firstY, secondY)
+    maxZ = Math.max(maxZ, firstZ, secondZ)
+    let brickLength = 0
+    let direction: Direction
+    if (firstEnd.x !== secondEnd.x) {
+      brickLength = firstEnd.x - secondEnd.x
+      direction = Direction.x
+    } else if (firstEnd.y !== secondEnd.y) {
+      brickLength = firstEnd.y - secondEnd.y
+      direction = Direction.y
+    } else {
+      brickLength = firstEnd.z - secondEnd.z
+      direction = Direction.z
+    }
+
+    const top = Math.max(firstZ, secondZ)
+    const bottom = direction === Direction.z ? Math.min(firstZ, secondZ) - 1 : top - 1
+    bricks.push({
+      top,
+      bottom,
+      firstEnd,
+      secondEnd,
+      direction,
+      brickLength: Math.abs(brickLength) + 1,
+    })
+  })
+  bricks.sort((a, b) => {
+    return a.top - b.top
+  })
+  return {bricks, maxX, maxY, maxZ}
+}
+
+export const dropABrick = (brick: Brick) => {
+  brick.top = brick.top - 1
+  brick.bottom = brick.bottom - 1
+  brick.firstEnd = {...brick.firstEnd, z: brick.firstEnd.z - 1}
+  brick.secondEnd = {...brick.secondEnd, z: brick.secondEnd.z - 1}
+  return brick
+}
+
+export const findSupportingBricks = (sortedBricks: Brick[], brick: Brick) => {
+  let supportingBricks = []
+  let currentLevel = brick.bottom
+
+  do {
+    const bricksBelow = sortedBricks.filter(sortedBrick => sortedBrick.top === currentLevel)
+
+    const brickAboveMaxY = Math.max(brick.firstEnd.y, brick.secondEnd.y)
+    const brickAboveMinY = Math.min(brick.firstEnd.y, brick.secondEnd.y)
+
+    const brickAboveMaxX = Math.max(brick.firstEnd.x, brick.secondEnd.x)
+    const brickAboveMinX = Math.min(brick.firstEnd.x, brick.secondEnd.x)
+
+    bricksBelow.forEach(brickBelow => {
+      const brickBelowMaxX = Math.max(brickBelow.firstEnd.x, brickBelow.secondEnd.x)
+      const brickBelowMinX = Math.min(brickBelow.firstEnd.x, brickBelow.secondEnd.x)
+      const brickBelowMaxY = Math.max(brickBelow.firstEnd.y, brickBelow.secondEnd.y)
+      const brickBelowMinY = Math.min(brickBelow.firstEnd.y, brickBelow.secondEnd.y)
+
+      if (brickBelowMaxY >= brickAboveMinY && brickBelowMinY <= brickAboveMaxY && brickBelowMaxX >= brickAboveMinX && brickBelowMinX <= brickAboveMaxX) {
+        supportingBricks.push(brickBelow)
+      }
+    })
+    if (supportingBricks.length === 0 && brick.bottom > 0) {
+      brick = dropABrick(brick)
+    }
+    currentLevel--
+  } while (supportingBricks.length === 0 && currentLevel > 0)
+  return supportingBricks
+}
+
+const brickToKey = (brick: Brick) => {
+  return `${brick.firstEnd.x}_${brick.firstEnd.y}_${brick.firstEnd.z}~${brick.secondEnd.x}_${brick.secondEnd.y}_${brick.secondEnd.z}`
+}
+
+export const letEmFall = (bricks: Brick[], maxZ: number) => {
+  const brickMap = new Map<string, BrickSupports>()
+  // start where top is 2, see if someone is below, if not, drop it on down.
+  let currentTop = 2
+  while (currentTop <= maxZ) {
+    const currentBricks = bricks.filter(brick => brick.top === currentTop)
+    currentBricks.forEach(currentBrick => {
+      const bricksBelow: Brick[] = findSupportingBricks(bricks, currentBrick)
+      currentBrick.supportingBricks = bricksBelow
+      bricksBelow.forEach(supporterBrick => {
+        const brickKey = brickToKey(supporterBrick)
+        if (brickMap.has(brickKey)) {
+          const brickSupport = brickMap.get(brickKey)
+          brickSupport.supportedBricks.push(currentBrick)
+          brickMap.set(brickKey, {
+            brick: supporterBrick,
+            supportedBricks: brickSupport.supportedBricks
+          })
+        } else {
+          brickMap.set(brickKey, {
+            brick: supporterBrick,
+            supportedBricks: [currentBrick]
+          })
+        }
+      })
+    })
+    currentTop++
+  }
+  return brickMap
+}
+
+export const identifyBricksToDisintegrate = (brickSupports: Map<string, BrickSupports>, allBricks: Brick[]) => {
+  const nonSupportiveBricksCount = allBricks.length - brickSupports.size
+  const keysToDisintegrate = []
+
+  const canDisintegrate = (brickSupport: BrickSupports) => {
+    const supportsZBrick = !!brickSupport.supportedBricks.filter(supportedBrick => {
+      return supportedBrick.direction === Direction.z
+    }).length
+
+    const allSupportedBricksSupportedByAnotherBrick = brickSupport.supportedBricks.every( (supportedBrick) => {
+      return supportedBrick.supportingBricks.length > 1
+    })
+    return !supportsZBrick && allSupportedBricksSupportedByAnotherBrick
+  }
+
+  brickSupports.forEach((value, key) => {
+    if (canDisintegrate(value)) {
+      keysToDisintegrate.push(key)
+    }
+  })
+  return keysToDisintegrate.length + nonSupportiveBricksCount
+}
 
 export const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
 
-  return
+  const brickSupportsMap = letEmFall(input.bricks, input.maxZ)
+  return identifyBricksToDisintegrate(brickSupportsMap, input.bricks)
 }
 
 export const part2 = (rawInput: string) => {
@@ -14,14 +188,27 @@ export const part2 = (rawInput: string) => {
   return
 }
 
-export const exampleInput = ``
+export const exampleInput = `1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9`
 
 run({
   part1: {
     tests: [
        {
          input: exampleInput,
-         expected: "",
+         expected: 5,
+       },
+       {
+         input: `0,0,1~0,0,2
+1,0,1~2,0,1
+1,0,2~1,0,2
+0,0,3~1,0,3`,
+         expected: 3,
        },
     ],
     solution: part1,
