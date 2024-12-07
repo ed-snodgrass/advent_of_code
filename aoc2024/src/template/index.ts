abfcd6256125927e11b58d3814c229b115837d66
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-06 23:35:02 -0600

    Revert "adding some template stuff to hopefully reduce how much copy pasta is necessary"
    This reverts commit 3c7566cea75808dfea84d51517547e855e8b34af.

--- aoc2024/src/template/index.ts
+++ aoc2024/src/template/index.ts
@@ -1,26 +1,38 @@
 import run from "aocrunner"
 
-import {part1, part2, exampleInputPart1, exampleInputPart2} from "./solution"
+const parseInput = (rawInput: string) => rawInput
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
 
 run({
   part1: {
     tests: [
-      {
-        input: exampleInputPart1,
-        expected: "",
-      },
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      {
-        input: exampleInputPart2,
-        expected: "",
-      },
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 })
