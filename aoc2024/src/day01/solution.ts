24cff848c2e5371c32407d24820c2087d59df50e
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-06 21:15:02 -0600

    day1 part1 pulling data

---
+++ aoc2024/src/day01/solution.ts
@@ -0,0 +1,22 @@
+export const parseInput = (rawInput: string) => rawInput
+
+export const exampleInputPart1 =  `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+
+export const exampleInputPart2 = exampleInputPart1
+
+export const part1 = (rawInput: string): number => {
+  const input = parseInput(rawInput)
+  console.log(input.split)
+  return 11
+}
+
+export const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
