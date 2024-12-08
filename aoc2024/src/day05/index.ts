7271036e2db4f9cd7fcc5afa12a6bfaa0aceb2cf
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-08 13:16:34 -0600

    day 5 setup

---
+++ aoc2024/src/day05/index.ts
@@ -0,0 +1,26 @@
+import run from "aocrunner"
+
+import {part1, part2, exampleInputPart1, exampleInputPart2} from "./solution.js"
+
+run({
+  part1: {
+    tests: [
+      {
+        input: exampleInputPart1,
+        expected: 143,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: exampleInputPart2,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
---
+++ aoc2024/src/day05/solution.ts
@@ -0,0 +1,48 @@
+export const parseInput = (rawInput: string) => {
+  const lines = rawInput.split('\n');
+
+  return lines
+}
+
+export const part1 = (rawInput: string):number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const part2 = (rawInput: string): number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const exampleInputPart1 =  `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+export const exampleInputPart2 = exampleInputPart1
