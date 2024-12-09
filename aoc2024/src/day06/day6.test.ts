ede61f665ef3af6c6d385685ab39cb95393e6674
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-08 21:44:26 -0600

    day 6 part 1

--- aoc2024/src/day06/day6.test.ts
+++ aoc2024/src/day06/day6.test.ts
@@ -11,15 +11,18 @@ describe('Day6Test tests', () => {
       })
     })
     describe('real input', () => {
-      it.skip('part1 should be...', () => {
-        expect(part1(input)).toBe(null)
+      it('part1 should be 5409', () => {
+        expect(part1(input)).toBe(5409)
+      })
+      it('part1 should be 5444', () => {
+        expect(part1(input)).toBeLessThan(5444)
       })
     })
   })
   describe('part2', () => {
     describe('example input', () => {
-      it.skip('part2 should be...', () => {
-        expect(part2(exampleInputPart2)).toBe(null)
+      it('part2 should be 6', () => {
+        expect(part2(exampleInputPart2)).toBe(6)
       })
     })
     describe('real input', () => {
--- aoc2024/src/day06/solution.ts
+++ aoc2024/src/day06/solution.ts
@@ -1,13 +1,62 @@
 export const parseInput = (rawInput: string) => {
-  const lines = rawInput.split('\n');
+  let startingPosition: {x: number, y: number} | undefined
+  const grid = rawInput.split('\n').map(line => line.split(''))
+  for (let i = 0; i < grid.length && !startingPosition; i++) {
+    for (let j = 0; j < grid[i].length && !startingPosition; j++) {
+      startingPosition = grid[i][j] === '^' ? {x: j, y: i} : undefined
+    }
+  }
+  return {startingPosition, grid}
+}
 
-  return lines
+const atEdge = (position: {x: number, y: number}, grid: string[][]) => {
+  return position.x === 0 || position.x === grid[0].length - 1 || position.y === 0 || position.y === grid.length - 1
+}
+const MAX_MOVES = 1000000
+
+const getNextPosition = (currentPosition: {x: number, y: number}, heading: '^'|'>'|'v'|'<'): {x: number, y: number} => {
+  switch (heading) {
+    case '<':
+      return {x: currentPosition.x - 1, y: currentPosition.y}
+    case '>':
+      return {x: currentPosition.x + 1, y: currentPosition.y}
+    case 'v':
+      return {x: currentPosition.x, y: currentPosition.y + 1}
+    case '^':
+      return {x: currentPosition.x, y: currentPosition.y - 1}
+    default:
+      return currentPosition
+  }
+}
+
+export const markGuardPath = (startingPosition: {x: number, y: number}, grid: string[][]) => {
+  let moves = 0
+  const visitedPositions: {x: number, y: number}[] = []
+  let currentPosition = startingPosition
+  visitedPositions.push(currentPosition)
+  let currentHeading: '^'|'>'|'v'|'<' = '^'
+  while (moves < MAX_MOVES && !atEdge(currentPosition, grid)) {
+    const nextPosition = getNextPosition(currentPosition, currentHeading)
+    if (grid[nextPosition.y][nextPosition.x] === '#') {
+      currentHeading = currentHeading === '^' ? '>' : currentHeading === '>' ? 'v' : currentHeading === 'v' ? '<' : '^'
+    } else {
+      moves++
+      if (grid[currentPosition.y][currentPosition.x] !== 'X') {
+        visitedPositions.push(currentPosition)
+        grid[currentPosition.y][currentPosition.x] = 'X'
+      }
+      currentPosition = nextPosition
+    }
+  }
+  grid[currentPosition.y][currentPosition.x] = currentHeading
+  // console.log(grid.map(line => line.join('')).join('\n'))
+  return visitedPositions
 }
 
 export const part1 = (rawInput: string):number => {
-  const input = parseInput(rawInput)
-
-  return -1
+  const {startingPosition, grid} = parseInput(rawInput)
+  const visitedPositions = markGuardPath(startingPosition!, grid)
+  return visitedPositions.length
 }
 
 export const part2 = (rawInput: string): number => {
