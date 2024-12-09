0c91e9ff94b028431dbbb15b206541458534570b
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-08 19:25:34 -0600

    day 6 setup

---
+++ aoc2024/src/day06/solution.ts
@@ -0,0 +1,30 @@
+export const parseInput = (rawInput: string) => {
+  const lines = rawInput.split('\n');
+
+  return lines
+}
+
+export const part1 = (rawInput: string):number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const part2 = (rawInput: string): number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const exampleInputPart1 =  `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
+
+export const exampleInputPart2 = exampleInputPart1
