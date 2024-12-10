9c021231e65da650f2f9842dd26dcf452dc24a84
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-09 18:54:18 -0600

    2024 day 7 setup

---
+++ aoc2024/src/day07/index.ts
@@ -0,0 +1,26 @@
+import run from "aocrunner"
+
+import {part1, part2, exampleInputPart1, exampleInputPart2} from "./solution.js"
+
+run({
+  part1: {
+    tests: [
+      {
+        input: exampleInputPart1,
+        expected: 3749,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: exampleInputPart2,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+})
---
+++ aoc2024/src/day07/solution.ts
@@ -0,0 +1,29 @@
+export const parseInput = (rawInput: string) => {
+  const lines = rawInput.split('\n');
+
+  return lines
+}
+
+export const part1 = (rawInput: string):number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const part2 = (rawInput: string): number => {
+  const input = parseInput(rawInput)
+
+  return -1
+}
+
+export const exampleInputPart1 =  `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+
+export const exampleInputPart2 = exampleInputPart1
