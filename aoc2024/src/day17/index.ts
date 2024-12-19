5dfbd230cc0cd5f4ac348dd83199ec7827eeb736
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-19 08:13:17 -0600

    2024 day 16-19 setup

---
+++ aoc2024/src/day17/index.ts
@@ -0,0 +1,40 @@
+import run from "aocrunner"
+
+//import {part1, part2, exampleInputPart1, exampleInputPart2} from "./solution.js"
+
+const parseInput = (rawInput: string) => rawInput
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+
+  return
+}
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: exampleInputPart1,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: exampleInputPart2,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
