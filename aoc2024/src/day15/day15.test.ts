0c55502aaaa172eb2a63f4f6978a19fcd2091225
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2024-12-20 20:41:17 -0600

    2024 day 15 part 1

--- aoc2024/src/day15/day15.test.ts
+++ aoc2024/src/day15/day15.test.ts
@@ -1,12 +1,126 @@
 import * as fs from "fs"
-import { part1, part2, parseInput, exampleInputPart1, exampleInputPart2, smallerInput } from "./solution"
+import {
+  part1,
+  part2,
+  parseInput,
+  exampleInputPart1,
+  exampleInputPart2,
+  smallerInput,
+  attemptMove,
+  DIRECTIONS, ROBOT, BOX, EMPTY,
+} from "./solution"
 
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
 describe('Day15Test tests', () => {
+  describe('parseInput', () => {
+    describe("smallerInput", () => {
+      it("should find a 8 x 8 map", () => {
+        expect(parseInput(smallerInput).warehouseMap.length).toBe(8)
+        expect(parseInput(smallerInput).warehouseMap[0].length).toBe(8)
+      })
+      it("should find a list of 15 movements", () => {
+        expect(parseInput(smallerInput).movements.length).toBe(15)
+      })
+      it("should find the robot @ 2,2", () => {
+        expect(parseInput(smallerInput).robotPosition).toEqual([2, 2])
+      })
+    })
+    describe("exampleInputPart1", () => {
+      it("should find an 8 x 8 map", () => {
+        expect(parseInput(exampleInputPart1).warehouseMap.length).toBe(10)
+        expect(parseInput(exampleInputPart1).warehouseMap[0].length).toBe(10)
+      })
+      it("should find a list of 700 movements", () => {
+        expect(parseInput(exampleInputPart1).movements.length).toBe(700)
+      })
+      it("should find the robot @ 4,4", () => {
+        expect(parseInput(exampleInputPart1).robotPosition).toEqual([4, 4])
+      })
+    })
+    describe("input", () => {
+      it("should find an 50 x 50 map", () => {
+        expect(parseInput(input).warehouseMap.length).toBe(50)
+        expect(parseInput(input).warehouseMap[0].length).toBe(50)
+      })
+      it("should find a list of 20000 movements", () => {
+        expect(parseInput(input).movements.length).toBe(20000)
+      })
+      it("should find the robot @ 24,24", () => {
+        expect(parseInput(input).robotPosition).toEqual([24, 24])
+      })
+    })
+  })
+  describe('attemptMove', () => {
+    let warehouseMap: string[][] = []
+
+    describe('when up move is unblocked', () => {
+      beforeEach(() => {
+        warehouseMap = [
+          ['#', '#', '#'],
+          ['#', '.', '#'],
+          ['#', '@', '#'],
+          ['#', '#', '#'],
+        ]
+      })
+      it('should return the robot in new position', () => {
+        expect(attemptMove(warehouseMap, [1, 2], DIRECTIONS['^']).newMap[1][1]).toBe(ROBOT)
+      })
+      it('should return the empty in old position', () => {
+        expect(attemptMove(warehouseMap, [1, 3], DIRECTIONS['^']).newMap[3][1]).toBe(EMPTY)
+      })
+    })
+    describe('when up move is blocked by wall', () => {
+      beforeEach(() => {
+        warehouseMap = [
+          ['#', '#', '#'],
+          ['#', '@', '#'],
+          ['#', '.', '#'],
+          ['#', '#', '#'],
+        ]
+      })
+      it('should return the robot in same position', () => {
+        expect(attemptMove(warehouseMap, [1, 1], DIRECTIONS['^']).newMap[1][1]).toBe(ROBOT)
+      })
+    })
+    describe('when up move is blocked by unblocked box', () => {
+      beforeEach(() => {
+        warehouseMap = [
+          ['#', '#', '#'],
+          ['#', '.', '#'],
+          ['#', 'O', '#'],
+          ['#', '@', '#'],
+          ['#', '#', '#'],
+        ]
+      })
+      it('should return the robot in new position', () => {
+        expect(attemptMove(warehouseMap, [1, 3], DIRECTIONS['^']).newMap[2][1]).toBe(ROBOT)
+      })
+      it('should return the box in new position', () => {
+        expect(attemptMove(warehouseMap, [1, 3], DIRECTIONS['^']).newMap[1][1]).toBe(BOX)
+      })
+      it('should return the empty in old position', () => {
+        expect(attemptMove(warehouseMap, [1, 3], DIRECTIONS['^']).newMap[3][1]).toBe(EMPTY)
+      })
+    })
+    describe('when up move is blocked by blocked box', () => {
+      beforeEach(() => {
+        warehouseMap = [
+          ['#', '#', '#'],
+          ['#', '#', '#'],
+          ['#', 'O', '#'],
+          ['#', '@', '#'],
+          ['#', '#', '#'],
+        ]
+      })
+      it('should return the same position', () => {
+        expect(attemptMove(warehouseMap, [1, 3], DIRECTIONS['^']).newMap[3][1]).toBe(ROBOT)
+      })
+    })
+  })
   describe('part1', () => {
-    describe('example input', () => {
-      it('part1 should be...', () => {
+    describe('smallerInput', () => {
+      it('part1 should be 2028', () => {
         expect(part1(smallerInput)).toBe(2028)
       })
     })
@@ -16,8 +130,8 @@ describe('Day15Test tests', () => {
       })
     })
     describe('real input', () => {
-      it.skip('part1 should be...', () => {
-        expect(part1(input)).toBe(null)
+      it('part1 should be 1568399', () => {
+        expect(part1(input)).toBe(1568399)
       })
     })
   })
--- aoc2024/src/day15/solution.ts
+++ aoc2024/src/day15/solution.ts
@@ -1,19 +1,132 @@
-export const parseInput = (rawInput: string) => {
-  const lines = rawInput.split('\n');
+export const WALL = '#'
+export const EMPTY = '.'
+export const BOX = 'O'
+export const ROBOT = '@'
 
-  return lines
+export type Direction = [number, number];
+
+export const RIGHT:Direction = [1, 0]
+export const DOWN:Direction = [0, 1]
+export const LEFT:Direction = [-1, 0]
+export const UP:Direction = [0, -1]
+
+export const DIRECTIONS = {
+  '^': UP,
+  '>': RIGHT,
+  'v': DOWN,
+  '<': LEFT,
+}
+
+export const parseInput = (rawInput: string) => {
+  const splitInput = rawInput.split('\n\n');
+  const warehouseMap = splitInput[0].split('\n').map(line => line.split(''))
+  const movements = splitInput[1].split('\n').join('').split('').map(movement => DIRECTIONS[movement as keyof typeof DIRECTIONS])
+
+  let robotPosition
+  for (let i = 0; i < warehouseMap.length; i++) {
+    const line = warehouseMap[i];
+    if (line.includes(ROBOT)) {
+      robotPosition = [line.indexOf(ROBOT), i]
+    }
+  }
+
+  return {warehouseMap, movements, robotPosition}
+}
+
+const findBoxesToMove = (warehouseMap: string[][], currentPosition: [number, number], direction: Direction): [number, number][]|undefined => {
+  const boxesToMove: [number, number][] = []
+  const [directionX, directionY] = direction
+
+  let [currentX, currentY] = currentPosition
+  let [newBoxX, newBoxY] = [currentX + directionX, currentY + directionY]
+  let nextItem = warehouseMap[newBoxY][newBoxX]
+
+  do {
+    nextItem = warehouseMap[newBoxY][newBoxX]
+    if (nextItem === WALL) {
+      return undefined
+    } else if (nextItem === EMPTY) {
+      boxesToMove.push(currentPosition)
+      return boxesToMove
+    } else {
+      boxesToMove.push(currentPosition)
+      currentPosition = [newBoxX, newBoxY]
+
+      currentX = currentPosition[0]
+      currentY = currentPosition[1]
+
+      newBoxX = currentX + directionX
+      newBoxY = currentY + directionY
+    }
+  } while (nextItem === BOX)
+  return boxesToMove
+}
+
+export const attemptMove = (warehouseMap: string[][], robotPosition: [number, number], direction: Direction) => {
+  const [directionX, directionY] = direction
+  const [currentRobotX, currentRobotY] = robotPosition
+  let [newRobotX, newRobotY] = [currentRobotX + directionX, currentRobotY + directionY]
+  const newPositionItem = warehouseMap[newRobotY][newRobotX]
+  const hasWallInNewPosition = newPositionItem === WALL
+
+  if (newPositionItem === BOX) {
+    const boxesToMove = findBoxesToMove(warehouseMap, [newRobotX, newRobotY], direction)
+    if (boxesToMove && boxesToMove.length) {
+      boxesToMove.forEach(([boxX, boxY], index) => {
+        warehouseMap[boxY + direction[1]][boxX + direction[0]] = BOX
+      })
+      warehouseMap[currentRobotY][currentRobotX] = EMPTY
+      warehouseMap[newRobotY][newRobotX] = ROBOT
+      // printGrid(warehouseMap)
+      return {newMap: warehouseMap, newPosition: [newRobotX, newRobotY] as [number, number]}
+    }
+  } else if (!hasWallInNewPosition) {
+    warehouseMap[currentRobotY][currentRobotX] = EMPTY
+    warehouseMap[newRobotY][newRobotX] = ROBOT
+    // printGrid(warehouseMap)
+    return {newMap: warehouseMap, newPosition: [newRobotX, newRobotY] as [number, number]}
+  }
+  return {newMap: warehouseMap, newPosition: robotPosition}
+}
+
+export const performAllMoves = (warehouseMap: string[][], robotPosition: [number, number], movements: Direction[]) => {
+  let newMap = warehouseMap
+  let newPosition: [number, number] = robotPosition
+  movements.forEach(movement => {
+    const moveResults = attemptMove(newMap, newPosition, movement)
+    newMap = moveResults.newMap
+    newPosition = moveResults.newPosition
+  })
+  return newMap
+}
+
+export const calculateGps = (warehouseMap: string[][]) => {
+  const boxes = []
+  for(let y = 0; y < warehouseMap.length; y++) {
+    for (let x = 0; x < warehouseMap[y].length; x++) {
+      if (warehouseMap[y][x] === BOX) boxes.push({x, y})
+    }
+  }
+  return boxes.map(box => (box.y * 100) + box.x).reduce((sum, value) => value + sum)
 }
 
 export const part1 = (rawInput: string):number => {
-  const input = parseInput(rawInput)
-
-  return -1
+  const {warehouseMap, movements, robotPosition} = parseInput(rawInput)
+  // console.log(`INITIAL`)
+  // printGrid(warehouseMap)
+  const finishedWarehouseMap = performAllMoves(warehouseMap, robotPosition as [number, number], movements)
+  // printGrid(finishedWarehouseMap)
+  return calculateGps(finishedWarehouseMap)
 }
 
 export const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput)
 
   return -1
+}
+
+export const printGrid = (grid: number[][]|string[][]) => {
+  console.log(grid.map(row => row.join('')).join('\n'))
 }
 
 export const smallerInput = `########
