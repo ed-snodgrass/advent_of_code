b21c53a6e8a759640cb02d801df5acec98e9dc04
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2025-12-04 23:16:30 -0600

    day4 part1 initial commit and isInCorner tests

--- aoc2025/src/day04/day04.test.ts
+++ aoc2025/src/day04/day04.test.ts
@@ -3,15 +3,54 @@ import {part1, part2, parseInput, exampleInputPart1, exampleInputPart2} from './
 
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
-describe.skip('Day04Test tests', () => {
+function isInCorner(grid: string[][], x: number, y: number) {
+  if (x === 0 && y === 0) {
+    return true
+  }
+  if (x === grid[0].length - 1 && y === 0) {
+    return true
+  }
+  if (x === 0 && y === grid.length - 1) {
+    return true
+  }
+  return x === grid[grid.length - 1].length - 1 && y === grid.length - 1;
+
+}
+
+function isBlocked(grid: string[][], x: number, y: number) {
+  if (isInCorner(grid, x, y)) {
+    return false
+  }
+  return undefined
+}
+
+describe('Day04Test tests', () => {
+  describe('isBlocked', () => {
+    let inputGrid: string[][]
+    beforeEach(() => {
+      inputGrid = parseInput(exampleInputPart1)
+    })
+    it('cannot not be blocked for roll in top left corner (y=0 and x=0)', () => {
+      expect(isBlocked(inputGrid, 0, 0)).toBe(false)
+    })
+    it('cannot not be blocked for roll in top right corner (y=0 and x=grid[0].length)', () => {
+      expect(isBlocked(inputGrid, inputGrid[0].length - 1, 0)).toBe(false)
+    })
+    it('cannot not be blocked for roll in bottom left corner (y=inputGrid.length and x=0)', () => {
+      expect(isBlocked(inputGrid, 0, inputGrid.length - 1)).toBe(false)
+    })
+    it('cannot not be blocked for roll in bottom right corner (y=inputGrid.length and x=inputGrid[inputGrid.length])', () => {
+      expect(isBlocked(inputGrid, inputGrid[inputGrid.length - 1].length - 1, inputGrid.length - 1)).toBe(false)
+    })
+  })
   describe('part1', () => {
     describe('example input', () => {
-      it('part1 should be...', () => {
-        expect(part1(exampleInputPart1)).toBe(null)
+      it('part1 should be 13', () => {
+        expect(part1(exampleInputPart1)).toBe(13)
       })
     })
-    describe('real input', () => {
-      it.skip('part1 should be...', () => {
+    describe.skip('real input', () => {
+      it('part1 should be...', () => {
         expect(part1(input)).toBe(null)
       })
     })
--- aoc2025/src/day04/solution.ts
+++ aoc2025/src/day04/solution.ts
@@ -1,12 +1,10 @@
 export const parseInput = (rawInput: string) => {
-  const lines = rawInput.split('\n');
-
-  return lines
+  return rawInput.split("\n").map((line) => line.split(""))
 }
 
 export const part1 = (rawInput: string):number => {
   const input = parseInput(rawInput)
-
+  console.log(input)
   return -1
 }
 
@@ -16,6 +14,15 @@ export const part2 = (rawInput: string): number => {
   return -1
 }
 
-export const exampleInputPart1 =  ``
+export const exampleInputPart1 =  `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`
 
 export const exampleInputPart2 = exampleInputPart1
