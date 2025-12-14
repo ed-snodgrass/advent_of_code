11ef12373c982d94364f52e658853d5ede1781c8
Ed Snodgrass <ed.snodgrass@aviture.us.com>
2025-12-14 09:53:23 -0600

    day 12 input parsing

--- aoc2025/src/day12/day12.test.ts
+++ aoc2025/src/day12/day12.test.ts
@@ -3,11 +3,29 @@ import {part1, part2, parseInput, exampleInputPart1, exampleInputPart2} from './
 
 const input = fs.readFileSync(`${__dirname}/input.txt`, 'utf-8');
 
-describe.skip('Day12Test tests', () => {
+describe('Day12Test tests', () => {
+  describe('parseInput', () => {
+    describe('with exampleInput', () => {
+      it('should find 6 shapes', () => {
+        expect(parseInput(exampleInputPart1).shapes.length).toBe(6)
+      })
+      it('should find 3 regions', () => {
+        expect(parseInput(exampleInputPart1).regions.length).toBe(3)
+      })
+    })
+    describe('with real input', () => {
+      it('should find 6 shapes', () => {
+        expect(parseInput(input).shapes.length).toBe(6)
+      })
+      it('should find 1000 regions', () => {
+        expect(parseInput(input).regions.length).toBe(1000)
+      })
+    })
+  })
   describe('part1', () => {
     describe('example input', () => {
-      it('part1 should be...', () => {
-        expect(part1(exampleInputPart1)).toBe(null)
+      it('part1 should find 2 regions 2', () => {
+        expect(part1(exampleInputPart1)).toBe(2)
       })
     })
     describe('real input', () => {
--- aoc2025/src/day12/solution.ts
+++ aoc2025/src/day12/solution.ts
@@ -1,7 +1,17 @@
 export const parseInput = (rawInput: string) => {
-  const lines = rawInput.trim().split('\n');
+  const groups = rawInput.trim().split('\n\n')
+  const shapes = groups.slice(0, groups.length - 1).map(shapeText => {
+    const shapeLines = shapeText.split('\n').slice(1, 4)
+    return shapeLines
+  })
+  const regions = groups[groups.length - 1].split('\n').map(regionText => {
+    const regionParts = regionText.split(':').map(text => text.trim())
+    const regionSize = regionParts[0].split('x').map(regionSizePart => Number(regionSizePart))
+    const shapeCounts = regionParts[1].split(' ').map(shapeCount => Number(shapeCount))
 
-  return lines
+    return {size: regionSize, shapeCounts}
+  })
+  return {shapes, regions}
 }
 
 export const part1 = (rawInput: string):number => {
@@ -16,6 +26,39 @@ export const part2 = (rawInput: string): number => {
   return -1
 }
 
-export const exampleInputPart1 =  ``
+export const exampleInputPart1 = `0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+`
 
 export const exampleInputPart2 = exampleInputPart1
